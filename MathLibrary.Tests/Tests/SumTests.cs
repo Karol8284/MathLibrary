2f1a488c009c042182a2d0ce5be49ea3012210b8
Karol8284 <93425066+Karol8284@users.noreply.github.com>
2026-02-13 19:09:46 +0100

    Poprawa pisowni

--- MathLibrary.Tests/Tests/SumTests.cs
+++ MathLibrary.Tests/Tests/SumTests.cs
@@ -12,5 +12,15 @@ namespace MathLibrary.Tests.Tests
         {
             MatyLibraryAssert.Sum(17, 2, 4, 5, 6);
         }
+        [Fact]
+        public void Sum_Double()
+        {
+            MatyLibraryAssert.Sum(17.0, 2.0, 4.0, 5.0, 6.0);
+        }
+        [Fact]
+        public void Sum_Float()
+        {
+            MatyLibraryAssert.Sum(17.0f, 2.0f, 4.0f, 5.0f, 6.0f);
+        }
     }
 }
